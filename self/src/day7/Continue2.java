5648274056bac03e9966628c650fef80a5157fad
조소현 <@So-426hyun>
2021-09-29 20:19:50 +0900

    day7

--- self/src/day7/Continue2.java
+++ self/src/day7/Continue2.java
@@ -7,7 +7,7 @@ public class Continue2 {
 			for(int j=0;j<=9;j++) {
 				System.out.print("j="+j);
 				if(j>2) continue;
-				System.out.print(출력됨\t");
+				System.out.print("출력됨\t");
 			}
 			System.out.println();
 		}
