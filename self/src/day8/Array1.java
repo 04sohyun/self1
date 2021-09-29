9ead41a09aca3888f75327de93c322372540061a
조소현 <@So-426hyun>
2021-09-29 20:33:45 +0900

    day8

---
+++ self/src/day8/Array1.java
@@ -0,0 +1,20 @@
+package day8;
+
+public class Array1 {
+	public static void main(String[] args) {
+		//1. 변수 선언 후 값 목록 대입
+		int[] intArr1;
+		intArr1 = new int[] {1,2,3,4};
+		
+		//2. 변수 선언과 동시에 초기값 대입
+		int[] intArr2 = {1,2,3,4,5};
+		
+		//3. 변수선언 후 객체 생성하여 대입
+		int[] intArr3;
+		intArr3 = new int[5];
+		
+		//4. 변수선언과 저장공간길이를 가지고 객체를 생성하여 대입
+		int[] intArr4 = new int[5];
+	}
+
+}
