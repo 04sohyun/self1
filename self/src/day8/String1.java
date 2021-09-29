c6e510e5f9a1fdad4cafea030aa3f223ef080552
조소현 <@So-426hyun>
2021-09-29 20:23:46 +0900

    day8

---
+++ self/src/day8/String1.java
@@ -0,0 +1,17 @@
+package day8;
+
+public class String1 {
+	public static void main(String[] args) {
+		//扁夯鸥涝
+		int a =10;
+		a+=5;//15
+		
+		//曼炼鸥涝
+		String str = "hello";
+		
+		str += "hi"; //"hellohi" 按眉积己, "hi"按眉积己
+		str += "bye";//"hellohibye" 按眉积己, "bye"按眉积己
+		System.out.println(str);
+	}
+
+}
