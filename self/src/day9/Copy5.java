bcfeb87badb88b418dd37d8c1f0bf30eb88cc5b8
조소현 <@So-426hyun>
2021-10-01 21:06:45 +0900

    day9

--- self/src/day9/Copy5.java
+++ self/src/day9/Copy5.java
@@ -7,12 +7,12 @@ public class Copy5 {
 		
 		int[] arr3 = new int[arr1.length+arr2.length];
 		
-		//1. arr1ÀÇ ¹è¿­ º¹»ç
+		//1. arr1ë°°ì—´ ë³µì‚¬
 		for(int i=0;i<arr1.length;i++) {
 			arr3[i]=arr1[i];
 		}
 		
-		//2. arr2ÀÇ ¹è¿­ º¹»ç
+		//2. arr2ë°°ì—´ ë³µì‚¬
 		for(int i=0;i<arr2.length;i++) {
 			arr3[arr1.length+i]=arr2[i];
 		}
