889d0e825ae24c8c574a756c3acb6ced4afe5f54
조소현 <@So-426hyun>
2021-10-02 18:33:36 +0900

    day11

---
+++ self/src/day11/SomeMethods1.java
@@ -0,0 +1,14 @@
+package day11;
+
+public class SomeMethods1 {
+
+	void run() {}
+	void stop() {}
+	void sound() {}
+	
+	void innerMethod() {
+		run();
+		stop();
+		sound();
+	}
+}
---
+++ self/src/day11/SomeMethods1Main.java
@@ -0,0 +1,12 @@
+package day11;
+
+public class SomeMethods1Main {
+	public static void main(String[] args) {
+		SomeMethods1 sm = new SomeMethods1();
+		
+		sm.run();
+		sm.stop();
+		sm.sound();
+	}
+
+}
