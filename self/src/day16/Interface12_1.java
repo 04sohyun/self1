50c1082dc456e1784c3600d5e55d22f2418728b6
조소현 <@So-426hyun>
2021-10-15 23:11:32 +0900

    day16

---
+++ self/src/day16/Interface12_1.java
@@ -0,0 +1,10 @@
+package day16;
+
+public class Interface12_1 {}
+
+class Animal{
+	String name;
+	void eat() {
+		System.out.println("∏‘¿Ã∏¶ ∏‘Ω¿¥œ¥Ÿ.");
+	}
+}
---
+++ self/src/day16/Interface12_2.java
@@ -0,0 +1,9 @@
+package day16;
+
+public class Interface12_2 {}
+
+class Bird extends Animal implements Interface12{
+	@Override
+	public void run() {}
+	//name필드와 eat()메소드 상속받음
+}
