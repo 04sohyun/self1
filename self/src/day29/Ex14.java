c599012837b107b5e793975a9e38a0fbca71335c
sohyun <sohyun@sohyun426>
2021-11-04 21:17:36 +0900

    day29

---
+++ self/src/day29/Ex14.java
@@ -0,0 +1,39 @@
+package day29;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Ex14 {
+	public static void main(String[] args) {
+		try {
+			//1.드라이버로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");	
+			//2.연결객체 생성
+			String url="jdbc:oracle:thin:@localhost:1521:xe";
+			String userid="madang";
+			String pwd="madang";
+			Connection con = DriverManager.getConnection(url,userid,pwd);
+			//3.쿼리전달객체 생성
+			Statement stmt = con.createStatement();
+			//4.쿼리문작성
+			String sql = "insert into customer values((select max(custid)+1 from customer),'홍길이',null,null)";
+			//5.쿼리문 전송 및 결과 받기
+			boolean isResult = stmt.execute(sql);
+			if(isResult) {
+				ResultSet rs = stmt.getResultSet();
+				while(rs.next()) {
+					System.out.println(rs.getInt(1)+","+rs.getString(2)+","+rs.getString(3));
+				}
+			}else {
+				int resultCount = stmt.getUpdateCount();
+				System.out.println("처리행의 수:"+resultCount);
+			}
+			//6.자원해제
+			stmt.close(); con.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
